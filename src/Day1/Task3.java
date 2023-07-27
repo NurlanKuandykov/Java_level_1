764f43e77c8ced8a7b30c8ed3e8179ae93048218
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-27 16:14:02 +0300

    Output the word "JAVA" to a column so that it repeats 10 times using a cycle for.

---
+++ src/Day1/Task3.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        for (int b = 0; b<=9; ++b){
+            System.out.println("JAVA");
+        }
+    }
+}
+
