5617e847ebe009f5a37544182c9cc8c76563e288
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-27 16:37:31 +0300

    Declared a variable of type int, variable name "k". Assigned the value 3 to this variable. Displayed lines like "b x k = ..." in a column, where "b" is a number that takes values from 1 to 9. Used the cycle while.

---
+++ src/Day1/Task6.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 3;
+        int b = 1;
+        while (b<=9){
+            System.out.println(b + " x " + k + " = " + (b*k));
+            b++;
+        }
+    }
+}
