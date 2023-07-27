a77f53cbcc2706228e80348d4d00c45d30fd8244
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-27 16:11:20 +0300

    Output the word "JAVA" to a string so that it repeats 10 times using a cycle for.

---
+++ src/Day1/Task2.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        for (int b = 0; b<=9; ++b){
+            System.out.print("JAVA ");
+        }
+    }
+}
+
