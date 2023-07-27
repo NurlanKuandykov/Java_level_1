3ab51bf693642c8c0923fa6fd2e9bd5d7c44ca21
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-27 15:59:10 +0300

    add Task1

---
+++ src/Day1/Task1.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a  = 10, b = 1;
+        while (b<=a){
+            System.out.print("JAVA ");
+            ++b;
+        }
+    }
+}
+
