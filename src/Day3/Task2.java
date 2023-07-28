a93eec9b7e99d13f58a198af467308bbf4683c97
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-28 17:03:08 +0300

    Implemented a program that takes two numbers as input from the user - a dividend and a divisor. For these two numbers, the program calculates the result of the division and prints it to the console. The program continues to accept dividend and divisor values from the user until the user enters 0 as divisor.

---
+++ src/Day3/Task2.java
@@ -0,0 +1,16 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            if (b == 0) break;
+
+            System.out.println(a / b);
+        }
+    }
+}
