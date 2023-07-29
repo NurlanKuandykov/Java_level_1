5972896fca5a1f43d495a7b78ad90195b5050c25
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-07-29 09:20:53 +0300

    From the keyboard, the user enters the number n - the size of the array. I created an array of the specified size and filled it with random numbers from 0 to 10. Then I printed the contents of the array to the console, and also printed information to the console about: 1) Array length 2) The number of numbers is greater than 8 3) The number of numbers equal to 1 4) Number of even numbers 5) Number of odd numbers 6) Sum of all array elements

---
+++ src/Day4/Task1.java
@@ -0,0 +1,37 @@
+package Day4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a random number - the size of the array");
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int a1 = 0;
+        int a2 = 0;
+        int a3 = 0;
+        int a4 = 0;
+        int a5 = 0;
+        for (int i = 0; i<x.length; i++){
+            Random r = new Random();
+            x[i] = r.nextInt(11);
+            if (x[i] > 8) ++a1;
+            if (x[i] == 1) ++a2;
+            if (x[i] % 2 == 0 && x[i] != 0) ++a3;
+            if (x[i] % 2 != 0) ++a4;
+            a5+=x[i];
+        }
+        for (int j = 0; j<x.length; j++){
+            System.out.print(x[j] + " ");
+        }
+        System.out.println();
+        System.out.println("Array length: " + n);
+        System.out.println("The number of numbers is greater than 8: " + a1);
+        System.out.println("The number of numbers equals to 1: " + a2);
+        System.out.println("Number of even numbers: " + a3);
+        System.out.println("Number of odd numbers: " + a4);
+        System.out.println("Sum of all array elements: " + a5);
+    }
+}
