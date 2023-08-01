18cb327cffc57c73bae15701f41c356ff04fddaf
NurlanKuandykov <nurlan_kuandykov@mail.ru>
2023-08-01 17:46:57 +0300

    Created a class Motorbike, with the fields “Year of manufacture”, “Color”, "Model". Created an instance of the class Motorbike using the constructor (I did not use setters). Adhered to the principles of encapsulation and used the keyword this. Using a getter, I got the year of manufacture, color, model, and displayed the values in the console.

---
+++ src/Day5/Task2.java
@@ -0,0 +1,8 @@
+package Day5;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2015, "Honda", "Blue");
+        System.out.println(motorbike.getModel() + "\n" + motorbike.getYear() + "\n" + motorbike.getColor());
+    }
+}
